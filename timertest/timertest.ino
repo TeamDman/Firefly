9aa6b3ceec4d16011338afbf3f90dc8e15207277
TeamDman <TeamDman9201@gmail.com>
2016-12-09 12:47:32 -0500

    IRRemote working with timers rather than ISR now

--- timertest/timertest.ino
+++ timertest/timertest.ino
@@ -1,7 +1,36 @@
+/*
+ Timer Interrupts Example
+
+ Demonstrates usage of the HardwareTimer classes by blinking the LED
+ 
+ Created 22 April 2010, last updated 8 June 2010
+ By Bryan Newbold for LeafLabs
+ This code is released with no strings attached.
+ 
+ */
+
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
 
 #include "pins.h"
+#include "domimg.h"
+
+#include <HT1632.h>
+#include <font_8x4.h>
+#include <images.h>
+
+
+
+#define LED_RATE 50    // in microseconds
+
+void handler_count1(void);
+void handler_count2(void);
 
 int pincheck = 0;
+IRrecv irrecv(3);
+decode_results results;
 
 void ButtonRight() {
     pincheck++;
@@ -19,14 +48,52 @@ void setup()
     pinMode(capButtonLeft,INPUT_PULLUP);
     attachInterrupt(capButtonLeft,ButtonLeft,FALLING);
     pinMode (pushButton, INPUT_PULLUP);
-
+    HT1632.begin(CS, SCK, MOSI);
+    irrecv.enableIRIn();
+    // Setup Counting Timers
+    Timer4.pause();
+    Timer4.setPrescaleFactor(1);
+    Timer4.setChannel1Mode(TIMER_OUTPUTCOMPARE);
+    Timer4.setOverflow(2287);
+    Timer4.setCompare1(300);
+    Timer4.attachCompare1Interrupt(IRInterrupt);
+    Timer4.setCompare2(200);
+    Timer4.attachCompare2Interrupt(handler2);
+    Timer4.resume();
 }
 
 void loop() {
+// STATE_IDLE      2
+// STATE_MARK      3
+// STATE_SPACE     4
+// STATE_STOP      5
+// STATE_OVERFLOW  6
     Serial.print("ir in ");
-    Serial.print(pincheck);
+    Serial.print(irparams.recvpin);
     Serial.print(" ||| ");
-    Serial.print(digitalRead(pincheck));//irparams.recvpin));
+    Serial.print(digitalRead(irparams.recvpin));
+    Serial.print(" ||| ");
+    Serial.print(irparams.rcvstate);
+    Serial.print(" ||| ");
+    if (irrecv.decode(&results)) {
+        Serial.print(results.value);
+        irrecv.resume();
+    } else {
+        Serial.print("no res");
+    }
 
+    if(digitalRead(pushButton)) {
+        Serial.print(" pause");
+        Timer4.pause();
+    } else {
+        Timer4.resume();
+    }
     Serial.print("\n");
+    delay(1);
+} 
+void handler2(void) {
+    HT1632.setPixelFF (random(16), random(12));
+    HT1632.clearPixelFF (random(16), random(12));
+    HT1632.clearPixelFF (random(16), random(12));
+    HT1632.render();
 } 
