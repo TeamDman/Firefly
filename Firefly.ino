30ba3882b3cc70400b55d7d46d9be913d1429c57
TeamDman <TeamDman9201@gmail.com>
2016-12-20 13:52:50 -0500

    Added gyro modes

--- Firefly.ino
+++ Firefly.ino
@@ -9,13 +9,10 @@
 #include <SparkFun_MMA8452Q.h>
 MMA8452Q accel;
 
-
-
 bool blockGameData[6][12];
 uint8_t blockGamePaddle=6;
 uint16_t blockGameHeight=3;
 uint8_t blockGamePosition=0;
-
 
 int i = 13; // start in middle
 uint8_t wdImage;
@@ -25,10 +22,39 @@ char disp [] = "suh dude";
 
 bool doUpdate = true;
 int16_t scollDelay = 0;
+#define threshold 0.004
 
-enum machineStates { idle, buttonPhysicalInput, buttonLeftInput, buttonRightInput, busy, sleeping } machineState;
-enum scrollModes { scrollStop, scrollLeft, scrollRight } scrollMode;
-enum displayModes { displayImage, displayText, displaySparkle, displayBlockGame } displayMode;
+enum directions {
+  none,
+  forward,
+  left,
+  right,
+  back
+}   direction;
+
+enum machineStates { 
+  idle, 
+  buttonPhysicalInput, 
+  buttonLeftInput, 
+  buttonRightInput, 
+  busy, 
+  sleeping
+}   machineState;
+
+enum scrollModes {
+  scrollStop,
+  scrollLeft,
+  scrollRight
+}   scrollMode;
+
+enum displayModes {
+  displayImage,
+  displayText,
+  displaySparkle,
+  displayGyro,
+  displayGameBlock,
+  displayGameGyro
+}   displayMode;
 
 void buttonPhysical(void) {
   machineState = buttonPhysicalInput;
@@ -46,6 +72,7 @@ void setup () {
 
   Serial.begin(115200);
   HT1632.begin(CS, SCK, MOSI);
+  accel.init();
 
   pinMode (pushButton, INPUT_PULLUP);
   pinMode (capButtonRight, INPUT_PULLUP);
@@ -70,10 +97,14 @@ void loop () {
     updateScrolling();
   }
 
-  if (displayMode==displayBlockGame) {
+  if (displayMode==displayGameBlock) {
     doModeGameBlock();
+  } else if ( displayMode == displayGameGyro ) {
+    doModeGameGyro();
   } else if ( displayMode == displaySparkle ) {
     doModeSparkle();
+  } else if ( displayMode == displayGyro ) {
+    doModeGyro();
   } else if ( displayMode == displayImage ) {
     doModeImage();
   } else if ( displayMode == displayText ) {
@@ -90,14 +121,18 @@ void loop () {
 void getNextMode() {
   Serial.println("\nbutton physical\n");
   if ( displayMode == displayImage ) {
-    displayMode = displayBlockGame;
+    displayMode = displayGameBlock;
     doUpdate=false;
     blockGameReset();
-  } else if (displayMode == displayBlockGame ) {
+  } else if (displayMode == displayGameBlock ) {
+    displayMode = displayGameGyro;
+  } else if ( displayMode == displayGameGyro) {
     displayMode = displaySparkle;
-    doUpdate=true;
     wd = wdText;
   } else if ( displayMode == displaySparkle  ) {
+    displayMode = displayGyro;
+    doUpdate=true;
+  } else if ( displayMode == displayGyro ) {
     displayMode = displayText;
   } else if ( displayMode == displayText ) {
     displayMode = displayImage;
@@ -240,5 +275,51 @@ void doModeGameBlock() {
     } else if (scrollMode == scrollLeft) {
       blockGamePosition--;
     }
+  }
+}
+
+void setPixel(uint8_t x, uint8_t y, directions dir) {
+  if (direction == dir) {
+    HT1632.setPixelFF(x,y);
+  }
+}
+
+
+void doModeGyro() {
+  if (accel.available()) {
+    accel.read();
+    calcDirection();
+    HT1632.clear();
+    HT1632.setPixelFF(2,11);
+
+    HT1632.setPixelFF(8+accel.cx*7,8);
+    HT1632.setPixelFF(7+accel.cx*7,8);
+    HT1632.setPixelFF(8+accel.cy*7,6);
+    HT1632.setPixelFF(7+accel.cy*7,6);
+    HT1632.setPixelFF(8+accel.cz*7,4);
+    HT1632.setPixelFF(7+accel.cz*7,4);
+
+    HT1632.setPixelFF(2,1);
+    setPixel(2,2,forward);
+    setPixel(2,0,back);
+    setPixel(3,1,right);
+    setPixel(1,1,left);
+  }
+}
+
+void doModeGameGyro() {
+  
+}
+
+void calcDirection() {
+  direction=none;
+  if (accel.cx > threshold && accel.cy > threshold) {
+    direction = forward;
+  } else if (accel.cx < -threshold && accel.cy < -threshold) {
+    direction = back;
+  } else if (accel.cx > threshold && accel.cy < -threshold) {
+    direction = right;
+  } else if (accel.cx < -threshold && accel.cy > threshold) {
+    direction = left;
   }
 }
